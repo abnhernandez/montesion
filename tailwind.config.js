ee76e19e3131963f993099ceaaeef82b047bf72c
Abner Daniel Hernández Ruiz <abnerhernandez.x@gmail.com>
2025-07-10 17:02:58 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -45,6 +45,22 @@ module.exports = {
           foreground: 'hsl(var(--destructive-foreground))',
         },
       },
+      keyframes: {
+        glow: {
+          '0%': {
+            boxShadow: '0 0 10px 2px #00d9ff66, 0 0 20px 3px #bf40bf44, 0 0 8px 1px #00ffaa88',
+          },
+          '50%': {
+            boxShadow: '0 0 18px 7px #00d9ffcc, 0 0 40px 10px #bf40bfee, 0 0 16px 6px #00ffaaee',
+          },
+          '100%': {
+            boxShadow: '0 0 8px 2px #00d9ff44, 0 0 16px 2px #bf40bf33, 0 0 3px 0px #00ffaa44',
+          },
+        },
+      },
+      animation: {
+        glow: 'glow 3s linear infinite alternate',
+      },
     },
   },
   plugins: [],
